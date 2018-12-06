f83113862f1e66c071b5e98f75bfe1eeced62d0b
Rachel McCart <rlmccart@ufl.edu>
2018-12-05 20:07:09 -0600

    Add sections and styling to html and css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // variables to select color
 
 const colorName = document.getElementsByTagName('p')[0];
-const colorSquare = document.querySelectorAll('square');
+const colorSquare = document.getElementsByClassName('square');
 
 //Create Placeholder Text 
 var fortuneTextPlaceholder = document.createTextNode('');
@@ -9,36 +9,3 @@ yourFortune.appendChild(fortuneTextPlaceholder);
 
 var magicFortunes = ['The fortune you seek is in another cookie', 'That is what she said', 'You will be hungry again in one hour', 'Loading', 'It is a good day to have a good day', 'That was not chicken', 'I am worth a fortune', 'Go ask your mom', 'You have rice in your teeth', 'No lucky numbers today']; 
 
-function wrapper() {
-
-  const flashColor = () => {
-    //colorBlock.classList.toggle('transparent');
-    console.log('hello');
-  };
-
-  const loopThrough = () => {
-    let colorSquareText = this.childNodes[0].nodeValue;
-    let SquaresTextLength = colorSquareText.length;
-
-    for (let i = 0; i < BlocksTextLength * 2; i++) {
-      setTimeout(function(){
-        flashColor();
-      }, i * 1000);
-    };
-
-  };
-  loopThrough();
-
-  //const hideSquare = () => {
-  //  colorSection.classList.toggle('hide');
-  //  numberSection.classList.toggle('hide');
-  //};
-
-  //hideSquare();
-
-
-};
-
-for(let i = 0; i < colorName.length; i++) {
-  colorName[i].addEventListener('click', wrapper);
-};
